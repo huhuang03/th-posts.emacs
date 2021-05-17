42713cfcef922a8bdd0eb14e8389fc710dec9470
york <york@duomai.com>
2021-05-17 08:58:24 +0800

    Initial

---
+++ th-posts.el
@@ -0,0 +1,24 @@
+;;; th-posts.el --- posts util
+
+;; Copyright (C) 2011 Free Software Foundation, Inc.
+
+;; Author: th <huhuagn03@gmail.com>
+;; Version: 0.1
+;; Package-Requires: (())
+;; Keywords: posts, blogs
+;; URL: https://github.com/huhuang03/th-posts.emacs
+
+
+;;; Commentary:
+
+;; This package provider some useful function to write a blog post.
+
+;;; Code:
+;;;###autoload
+(define-minor-mode th-posts-mode
+  ())
+
+;; what should I do?
+
+(provide 'th-posts-mode)
+
